1bcd25ffa1ba27aa5768a258ade3525a2b97757b
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-12 20:42:30 +0000

    Adds Borrower name property with getName method, passing test.

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,9 +1,16 @@
 public class Borrower {
 
-    public Borrower(){
+    private String name;
 
-        
+    public Borrower(String name){
 
+        this.name = name;
+
+    }
+
+
+    public String getName(){
+        return this.name;
     }
 
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,20 +1,29 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BorrowerTest {
 
     private Borrower borrower;
     private Library library;
     private Book book1;
-    private Book book2
+    private Book book2;
 
     @Before
     public void before(){
 
-        borrower = new Borrower();
+        borrower = new Borrower("Jamie");
         library = new Library(5);
         book1 = new Book("Waylander", "David Gemmell", "Fantasy Fiction");
         book2 = new Book("Waylander 2", "David Gemmell", "Fantasy Fiction");
 
+    }
+
+
+    @Test
+    public void canGetName(){
+        assertEquals("Jamie", borrower.getName());
     }
 
 }
