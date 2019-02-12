13443a58feb8a60aa9c4e93e9b2e067bc49bfe5f
Jamie Reid <wjreid85.jr@gmail.com>
2019-02-12 17:21:29 +0000

    Adds empty bookCollection to Library class and bookCollectionCount, passing test.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> bookCollection;
 
     public Library(){
 
+        this.bookCollection = new ArrayList<>();
+
+    }
 
 
+    public int bookCollectionCount(){
+        return this.bookCollection.size();
     }
 
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
@@ -8,24 +11,27 @@ public class LibraryTest {
     Book book1;
     Book book2;
     Book book3;
-    ArrayList<Book> bookCollection;
+    ArrayList<Book> books;
 
     @Before
     public void before(){
 
         library = new Library();
-        bookCollection = new ArrayList<>();
+        books = new ArrayList<>();
 
         book1 = new Book("Waylander", "David Gemmell", "Fantasy Fiction");
         book2 = new Book("Waylander 2", "David Gemmell", "Fantasy Fiction");
         book3 = new Book("The Ill Made Knight", "Christian Cameron", "Medieval Fiction");
-        bookCollection.add(book1);
-        bookCollection.add(book2);
-        bookCollection.add(book3);
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
 
     }
 
 
-
+    @Test
+    public void hasABookCollectionStartsEmpty(){
+        assertEquals(0, library.bookCollectionCount());
+    }
 
 }
